5015ebe9c8e4fc03d453946185cb139901916b33
JimYYM <102501435+NTECAI@users.noreply.github.com>
2024-04-10 14:46:32 +0800

    u

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ else:
 # Function to load a specified model
 def load_model(model_name):
     model = torch.load(f'models/{model_name}')
+    model = model_dict['model']
     model.eval()
     return model
 
